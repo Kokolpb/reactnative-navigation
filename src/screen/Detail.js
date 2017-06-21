b8cc6a45077db6bcc2ab5df8084f0ac81e4693f8
enqtran <enqtran@gmail.com>
2017-06-21 16:46:14 +0700

    update

--- src/screen/Detail.js
+++ src/screen/Detail.js
@@ -3,6 +3,7 @@ import { Text, View, TouchableOpacity } from 'react-native';
 
 export default class Detail extends Component {
     render() {
+        const { params } = this.props.navigation.state;
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>
@@ -18,8 +19,8 @@ export default class Detail extends Component {
                     <Text style={{color:'#fff', backgroundColor:'#333',padding:10,fontSize:18,borderRadius:5, alignItems:'center'}}>Back</Text>
                 </TouchableOpacity>
 
-                <Text>{this.props.navigation.state.params.data}</Text>
-                <Text>{this.props.navigation.state.params.description}</Text>
+                <Text>{params.data}</Text>
+                <Text>{params.description}</Text>
             </View>
         );
     }
