35120347a5c19b2c6908a9107ad9f1d31e726adf
enqtran <enqtran@gmail.com>
2017-06-22 17:14:21 +0700

    flatlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { SideMenu } from './Router';
+// import { SideMenu } from './Router';
+import MainGif from './MainGif';
 
 export default class App extends Component {
 	render() {
 		return (
-			<SideMenu />
+			<MainGif />
 		);
 	}
 }
