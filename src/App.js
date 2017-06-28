5acbc36c35cb005f9e7183974be714d13ff8826e
enqtran <enqtran@gmail.com>
2017-06-28 18:19:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@
  */ 
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { SideMenu } from './Router';
+// import { SideMenu } from './Router';
+import Login from './screen/Login';
 
 export default class App extends Component {
 	render() {
 		return (
-			<SideMenu />
+			<Login />
 		);
 	}
 }
